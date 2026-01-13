7de1b356b2df9facfebcf6f7cfdb6a15828c3a37
Christisn-J <christian.jetter@student.uni-tuebingen.de>
2026-01-13 13:22:12 +0100

    Remove the written-out tillation equation.

--- include/sph/pressure.cuh
+++ include/sph/pressure.cuh
@@ -54,7 +54,6 @@ namespace EOS {
      * @param particles Particles class instance
      * @param index Relevant particle index
      */
-//    __device__ void tillotson(Material *materials, Particles *particles, int index);
     __device__ void tillotson(Material *materials, Particles *particles, int index, double rho, double e, double &eta, double &mu, double &p1, double &p2);
 
     /**
