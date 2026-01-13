eeb90d109480c849a82c2391c35f4ea2c9f8dd74
Christisn-J <christian.jetter@student.uni-tuebingen.de>
2026-01-13 13:23:14 +0100

    Adjustments to help functions and variables

--- src/cuda_utils/cuda_utilities.cu
+++ src/cuda_utils/cuda_utilities.cu
@@ -18,19 +18,15 @@ __device__ double atomicAdd(double* address, double val) {
 }
 #endif
 
-void gpuAssert(cudaError_t code, const char *file, int line, bool abort)
-{
-    if (code != cudaSuccess)
-    {
-        fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+void gpuAssert(cudaError_t code, const char *file, int line, bool abort) {
+    if (code != cudaSuccess) {
+        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
         if (abort) getchar();
     }
 }
 
-void checkCudaCall(cudaError_t command, const char * commandName, const char * fileName, int line)
-{
-    if (command != cudaSuccess)
-    {
+void checkCudaCall(cudaError_t command, const char *commandName, const char *fileName, int line) {
+    if (command != cudaSuccess) {
         fprintf(stderr, "Error: CUDA result \"%s\" for call \"%s\" in file \"%s\" at line %d. Terminating...\n",
                 cudaGetErrorString(command), commandName, fileName, line);
         exit(0);
@@ -66,14 +62,14 @@ namespace CudaUtils {
         }
 
         __global__ void checkValues(integer *indices, real *entry1, real *entry2, real *entry3,
-                                      integer count) {
+                                    integer count) {
 
             integer index = threadIdx.x + blockIdx.x * blockDim.x;
             integer stride = blockDim.x * gridDim.x;
             integer offset = 0;
 
             while ((index + offset) < count) {
-                for (int i=0; i<count; i++) {
+                for (int i = 0; i < count; i++) {
                     if (i != index + offset) {
                         if (entry1[indices[i]] == entry1[indices[index + offset]] && entry2[indices[i]] == entry2[indices[index + offset]]) {
                             printf("Same entry for %i vs %i | %i vs %i!\n", i, index + offset, indices[i], indices[index + offset]);
@@ -92,7 +88,7 @@ namespace CudaUtils {
             integer offset = 0;
 
             while ((index + offset) < length) {
-                for (int i=0; i<length; i++) {
+                for (int i = 0; i < length; i++) {
                     if (i != (index + offset)) {
                         if (array[index + offset] == array[i]) {
                             atomicAdd(duplicateCounter, 1);
@@ -111,7 +107,7 @@ namespace CudaUtils {
 
             while ((bodyIndex + offset) < length) {
 
-                for (int i=0; i<length; i++) {
+                for (int i = 0; i < length; i++) {
                     if (i != (bodyIndex + offset)) {
                         if (array1[bodyIndex + offset] == array1[i] && array2[bodyIndex + offset] == array2[i]) {
                             atomicAdd(duplicateCounter, 1);
@@ -132,10 +128,10 @@ namespace CudaUtils {
 
             while ((bodyIndex + offset) < length) {
 
-                for (int i=0; i<length; i++) {
+                for (int i = 0; i < length; i++) {
                     if (i != (bodyIndex + offset)) {
                         if (array1[bodyIndex + offset] == array1[i] && array2[bodyIndex + offset] == array2[i] &&
-                                array3[bodyIndex + offset] == array3[i]) {
+                            array3[bodyIndex + offset] == array3[i]) {
 
                             atomicAdd(duplicateCounter, 1);
 
@@ -158,10 +154,10 @@ namespace CudaUtils {
                 if ((index + offset) % 100 == 0) {
                     //printf("array[%i] = %i\n", index+offset, array[index + offset]);
                 }
-                for (int i=0; i<length; i++) {
+                for (int i = 0; i < length; i++) {
                     if (i != (index + offset)) {
                         if (array[index + offset] == array[i] || (entry1[array[index + offset]] == entry1[array[i]] && entry2[array[index + offset]] == entry2[array[i]])) {
-                        //if (array[index + offset] == array[i]) { //|| (entry1[index + offset] = entry1[i] && entry2[index + offset] == entry2[i])) {
+                            //if (array[index + offset] == array[i]) { //|| (entry1[index + offset] = entry1[i] && entry2[index + offset] == entry2[i])) {
                             printf("Found duplicate!\n");
                             atomicAdd(duplicateCounter, 1);
                         }
@@ -184,7 +180,7 @@ namespace CudaUtils {
                 if (array[index + offset] != -1) {
                     for (integer i = 0; i < length; i++) {
                         if (i != (index + offset)) {
-                            if (array[i] != -1 &&  array[index + offset] == array[i]) {
+                            if (array[i] != -1 && array[index + offset] == array[i]) {
 
                                 printf("DUPLICATE: %i vs %i (%i vs. %i)\n",
                                        array[index + offset], array[i], index + offset, i);
@@ -264,14 +260,14 @@ namespace CudaUtils {
                                            entry1[array[index + offset]], entry2[array[index + offset]], entry3[array[index + offset]],
                                            entry1[array[i]], entry2[array[i]], entry3[array[i]]);
 
-                                    for (int k=0; k<POW_DIM; k++) {
-                                        if (child[POW_DIM*array[index + offset] + k] == array[i]) {
+                                    for (int k = 0; k < POW_DIM; k++) {
+                                        if (child[POW_DIM * array[index + offset] + k] == array[i]) {
                                             printf("isChild: index = %i: child %i == i = %i\n", array[index + offset],
                                                    k, array[i]);
                                             isChild = true;
                                         }
 
-                                        if (child[8*array[i] + k] == array[index + offset]) {
+                                        if (child[8 * array[i] + k] == array[index + offset]) {
                                             printf("isChild: index = %i: child %i == index = %i\n", array[i],
                                                    k, array[index + offset]);
                                             isChild = true;
@@ -353,8 +349,10 @@ namespace CudaUtils {
             return cuda::launch(true, executionPolicy, ::CudaUtils::Kernel::findDuplicates<T>, array,
                                 duplicateCounter, length);
         }
+
         // explicit instantiation for type "integer"
         template real Launch::findDuplicates<integer>(integer *array, integer *duplicateCounter, int length);
+
         // explicit instantiation for type "real"
         template real Launch::findDuplicates<real>(real *array, integer *duplicateCounter, int length);
 
@@ -364,6 +362,7 @@ namespace CudaUtils {
             return cuda::launch(true, executionPolicy, ::CudaUtils::Kernel::findDuplicateEntries<T>, array1,
                                 array2, duplicateCounter, length);
         }
+
         template real Launch::findDuplicateEntries<real>(real *array1, real *array2, integer *duplicateCounter, int length);
 
         template<typename T>
@@ -372,6 +371,7 @@ namespace CudaUtils {
             return cuda::launch(true, executionPolicy, ::CudaUtils::Kernel::findDuplicateEntries<T>, array1,
                                 array2, array3, duplicateCounter, length);
         }
+
         template real Launch::findDuplicateEntries<real>(real *array1, real *array2, real *array3, integer *duplicateCounter, int length);
 
         template<typename T, typename U>
@@ -380,8 +380,9 @@ namespace CudaUtils {
             return cuda::launch(true, executionPolicy, ::CudaUtils::Kernel::findDuplicates<T, U>, array, entry1, entry2,
                                 duplicateCounter, length);
         }
+
         template real Launch::findDuplicates<integer, real>(integer *array, real *entry1, real *entry2,
-                integer *duplicateCounter, int length);
+                                                            integer *duplicateCounter, int length);
 
         template<typename T>
         real Launch::markDuplicates(T *array, integer *duplicateCounter, int length) {
@@ -389,8 +390,10 @@ namespace CudaUtils {
             return cuda::launch(true, executionPolicy, ::CudaUtils::Kernel::markDuplicates<T>, array,
                                 duplicateCounter, length);
         }
+
         // explicit instantiation for type "integer"
         template real Launch::markDuplicates<integer>(integer *array, integer *duplicateCounter, int length);
+
         // explicit instantiation for type "real"
         template real Launch::markDuplicates<real>(real *array, integer *duplicateCounter, int length);
 
@@ -408,6 +411,7 @@ namespace CudaUtils {
             return cuda::launch(true, executionPolicy, ::CudaUtils::Kernel::markDuplicates<T, U>, array, entry1, entry2,
                                 entry3, duplicateCounter, child, length);
         }
+
         template real Launch::markDuplicates<integer, real>(integer *array, real *entry1, real *entry2, real *entry3, integer *duplicateCounter, integer *child, int length);
 
 
@@ -417,12 +421,14 @@ namespace CudaUtils {
             return cuda::launch(true, executionPolicy, ::CudaUtils::Kernel::removeDuplicates<T>, array, removedArray,
                                 duplicateCounter, length);
         }
+
         // explicit instantiation for type "integer"
         template real Launch::removeDuplicates<integer>(integer *array, integer *removedArray,
-                integer *duplicateCounter, int length);
+                                                        integer *duplicateCounter, int length);
+
         // explicit instantiation for type "real"
         template real Launch::removeDuplicates<real>(real *array, real *removedArray,
-                integer *duplicateCounter, int length);
+                                                     integer *duplicateCounter, int length);
     }
 
 }
@@ -487,7 +493,7 @@ namespace cuda {
 #endif
         }
 
-        __device__ real pow(real a, real b){
+        __device__ real pow(real a, real b) {
 #if SINGLE_PRECISION
             return powf(a, b);
 #else
@@ -495,4 +501,44 @@ namespace cuda {
 #endif
         }
     }
-}
+
+    namespace util {
+        __device__ inline bool isNan(real x) {
+#if SINGLE_PRECISION
+            return __isnanf(x);   // float
+#else
+            return __isnan(x);    // double
+#endif
+        }
+
+        __device__ inline bool isInf(real x) {
+#if SINGLE_PRECISION
+            return __isinff(x);   // float
+#else
+            return __isinf(x);    // double
+#endif
+        }
+
+        __device__ inline bool isOverflow(real x) {
+#if SINGLE_PRECISION
+            const real limit = Constants::numerics::dbl_max * 0.9f;
+            return x > limit || x < -limit;
+#else
+            const real limit = Constants::numerics::dbl_max * 0.9;
+            return x > limit || x < -limit;
+#endif
+        }
+
+        __device__ inline bool isUnderflow(real x) {
+#if SINGLE_PRECISION
+            const real tiny = Constants::numerics::dbl_min;
+            return (x != 0.0f && fabsf(x) < tiny);
+#else
+            const real tiny = Constants::numerics::dbl_min;
+            return (x != 0.0 && fabs(x) < tiny);
+#endif
+        }
+
+    } // namespace util
+} // namespace cuda
+
