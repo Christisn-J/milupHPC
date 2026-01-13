651921ea4b4de176226073561276b7147925ed63
Christisn-J <christian.jetter@student.uni-tuebingen.de>
2026-01-13 13:29:14 +0100

    adapt to structural plasticity

--- src/sph/plasticity.cu
+++ src/sph/plasticity.cu
@@ -4,7 +4,8 @@ __device__ void SPH::applyPlasticity(Material *materials, int matId, Particles *
 #if PLASTICITY
     double mises_f = 1.0;
     double J2 = 0.0;
-    double y_0 = materials[matId].eos.yield_stress;
+    double y_0 = materials[matId].plasticity.yield_stress;
+//    double y_0 = materials[matId].eos.yield_stress;
 
 // --- Access deviatoric stress tensor components ---
 #if DIM >= 1
@@ -61,56 +62,58 @@ if (J2 > 0.0) {
 
 
 __device__ void SPH::calcPlasticity(Material *materials, int matId, Particles *particles, real sigma[DIM][DIM], int index) {
-    double mises_f = 1.0;
-    double J2 = 0.0;
-    double y;
-
-    double trace = 0.0;
-
-    #if DIM == 1
-        trace = sigma[0][0];
-        J2 = 0.5 * ((sigma[0][0] - trace) * (sigma[0][0] - trace));
-    #elif DIM == 2
-        trace = 0.5 * (sigma[0][0] + sigma[1][1]);
-        J2 = 0.5 * (
-            (sigma[0][0] - trace) * (sigma[0][0] - trace) +
-            (sigma[1][1] - trace) * (sigma[1][1] - trace) +
-            2.0 * sigma[0][1] * sigma[0][1]
-        );
-    #elif DIM == 3
-        trace = (sigma[0][0] + sigma[1][1] + sigma[2][2]) / 3.0;
-        J2 = 0.5 * (
-            (sigma[0][0] - trace) * (sigma[0][0] - trace) +
-            (sigma[1][1] - trace) * (sigma[1][1] - trace) +
-            (sigma[2][2] - trace) * (sigma[2][2] - trace) +
-            2.0 * (
-                sigma[0][1] * sigma[0][1] +
-                sigma[0][2] * sigma[0][2] +
-                sigma[1][2] * sigma[1][2]
-            )
-        );
-    #endif
-
-    y = materials[matId].eos.yield_stress;
-    if (J2 > 0.0) {
-        mises_f = (y * y) / (3.0 * J2);
-    }
-    if (mises_f > 1.0) {
-        mises_f = 1.0;
-    }
-
-    #if DIM >= 1
-        sigma[0][0] = mises_f * (sigma[0][0] - trace) + trace;
-    #endif
-    #if DIM >= 2
-        sigma[1][1] = mises_f * (sigma[1][1] - trace) + trace;
-        sigma[0][1] = sigma[1][0] = mises_f * sigma[0][1];
-    #endif
-    #if DIM >= 3
-        sigma[2][2] = mises_f * (sigma[2][2] - trace) + trace;
-        sigma[0][2] = sigma[2][0] = mises_f * sigma[0][2];
-        sigma[1][2] = sigma[2][1] = mises_f * sigma[1][2];
-    #endif
-
+#if PLASTICITY
+//    double mises_f = 1.0;
+//    double J2 = 0.0;
+//    double y;
+//
+//    double trace = 0.0;
+//
+//    #if DIM == 1
+//        trace = sigma[0][0];
+//        J2 = 0.5 * ((sigma[0][0] - trace) * (sigma[0][0] - trace));
+//    #elif DIM == 2
+//        trace = 0.5 * (sigma[0][0] + sigma[1][1]);
+//        J2 = 0.5 * (
+//            (sigma[0][0] - trace) * (sigma[0][0] - trace) +
+//            (sigma[1][1] - trace) * (sigma[1][1] - trace) +
+//            2.0 * sigma[0][1] * sigma[0][1]
+//        );
+//    #elif DIM == 3
+//        trace = (sigma[0][0] + sigma[1][1] + sigma[2][2]) / 3.0;
+//        J2 = 0.5 * (
+//            (sigma[0][0] - trace) * (sigma[0][0] - trace) +
+//            (sigma[1][1] - trace) * (sigma[1][1] - trace) +
+//            (sigma[2][2] - trace) * (sigma[2][2] - trace) +
+//            2.0 * (
+//                sigma[0][1] * sigma[0][1] +
+//                sigma[0][2] * sigma[0][2] +
+//                sigma[1][2] * sigma[1][2]
+//            )
+//        );
+//    #endif
+//
+//    y = materials[matId].plasticity.yield_stress;
+//
+//    if (J2 > 0.0) {
+//        mises_f = (y * y) / (3.0 * J2);
+//    }
+//    if (mises_f > 1.0) {
+//        mises_f = 1.0;
+//    }
+//
+//    #if DIM >= 1
+//        sigma[0][0] = mises_f * (sigma[0][0] - trace) + trace;
+//    #endif
+//    #if DIM >= 2
+//        sigma[1][1] = mises_f * (sigma[1][1] - trace) + trace;
+//        sigma[0][1] = sigma[1][0] = mises_f * sigma[0][1];
+//    #endif
+//    #if DIM >= 3
+//        sigma[2][2] = mises_f * (sigma[2][2] - trace) + trace;
+//        sigma[0][2] = sigma[2][0] = mises_f * sigma[0][2];
+//        sigma[1][2] = sigma[2][1] = mises_f * sigma[1][2];
+//    #endif
+#endif
 }
 
