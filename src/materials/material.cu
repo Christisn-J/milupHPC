46aad591ce79109a3b6598f0a8a791cc82373ebe
Christisn-J <christian.jetter@student.uni-tuebingen.de>
2026-01-13 13:25:10 +0100

    customizing info()

--- src/materials/material.cu
+++ src/materials/material.cu
@@ -1,84 +1,103 @@
 #include "../../include/materials/material.cuh"
 #include "../../include/cuda_utils/cuda_launcher.cuh"
 
-CUDA_CALLABLE_MEMBER Material::Material() {
+/**
+ * @brief Default constructor.
+ */
+CUDA_CALLABLE_MEMBER Material::Material() {}
 
-}
+/**
+ * @brief Destructor.
+ *
+ * This destructor is intentionally left empty because all member variables
+ * of Material are either primitive types or trivially destructible structures.
+ */
+CUDA_CALLABLE_MEMBER Material::~Material() {}
 
-CUDA_CALLABLE_MEMBER Material::~Material() {
-
-}
-
+/**
+ * @brief Print material information to console.
+ *
+ * Outputs all material properties, including optional parameters
+ * depending on compile-time flags (ARTIFICIAL_VISCOSITY, ARTIFICIAL_STRESS, PLASTICITY).
+ *
+ * @note TODO: Extend this function to include any additional material parameters.
+ */
 CUDA_CALLABLE_MEMBER void Material::info() {
-    // TODO: Maybe add switch or #if, #else, ... to only print relevant paramters
+//TODO: Consider adding conditionals or #if/#else to print only relevant parameters.
     printf("-------------------------- Material Information --------------------------\n");
+#if CHECK_MATERIAL
+    printf("Material: integer                                    = %i\n", dummy_int);
+    printf("Material: real                                       = %f\n", dummy_real);
+#endif
     printf("Material: ID                                        = %i\n", ID);
     printf("Material: interactions                              = %i\n", interactions);
+
+#if ARTIFICIAL_VISCOSITY
     // Artificial Viscosity Parameters
-    printf("Material: alpha                                     = %f\n", artificialViscosity.alpha);
-    printf("Material: beta                                      = %f\n", artificialViscosity.beta);
-    // EOS Parameters
-    printf("Material: eos: type                                 = %i\n", eos.type);
-    printf("Material: eos: polytropic_K                         = %f\n", eos.polytropic_K);
-    printf("Material: eos: polytropic_gamma                     = %f\n", eos.polytropic_gamma);
-    printf("Material: eos: rho0                                 = %f\n", eos.rho_0);
-    printf("Material: eos: bulk_modulus                         = %f\n", eos.bulk_modulus);
-    printf("Material: eos: n                                    = %f\n", eos.n);
-    printf("Material: eos: shear_modulus                        = %f\n", eos.shear_modulus);
-    printf("Material: eos: young_modulus                        = %f\n", eos.young_modulus);
-    printf("Material: eos: yield_stress                         = %f\n", eos.yield_stress);
-    // Artificial Stress Parameters
+    printf("Material: artificial Viscosity: alpha               = %f\n", artificialViscosity.alpha);
+    printf("Material: artificial Viscosity: beta                = %f\n", artificialViscosity.beta);
+#endif
+
 #if ARTIFICIAL_STRESS
+    // Artificial Stress Parameters
     printf("Material: artificial Stress: exponent tensor        = %f\n", artificialStress.exponent_tensor);
     printf("Material: artificial Stress: epsilon                = %f\n", artificialStress.epsilon_stress);
     printf("Material: artificial Stress: mean particle distance = %f\n", artificialStress.mean_particle_distance);
 #endif
-    // Tillotson EOS Parameters
-    printf("Material: eos: till_A                                = %f\n", eos.till_A);
-    printf("Material: eos: till_B                                = %f\n", eos.till_B);
-    printf("Material: eos: E_0                                   = %f\n", eos.E_0);
-    printf("Material: eos: E_iv                                  = %f\n", eos.E_iv);
-    printf("Material: eos: E_cv                                  = %f\n", eos.E_cv);
-    printf("Material: eos: till_a                                = %f\n", eos.till_a);
-    printf("Material: eos: till_b                                = %f\n", eos.till_b);
-    printf("Material: eos: till_alpha                            = %f\n", eos.till_alpha);
-    printf("Material: eos: till_beta                             = %f\n", eos.till_beta);
-    printf("Material: eos: rho_limit                             = %f\n", eos.rho_limit);
-    printf("Material: eos: cs_limit                              = %f\n", eos.cs_limit);
-    // TODO: add other parameters
+
+#if PLASTICITY
+    // Plasticity Parameters
+    printf("Material: plasticity: yield_stress                  = %f\n", plasticity.yield_stress);
+//    printf("Material: eos: yield_stress                         = %f\n", eos.yield_stress);
+#endif
+
+    // Equation of State Parameters
+    printf("Material: eos: type                                 = %i\n", eos.type);
+    printf("Material: eos: rho0                                 = %f\n", eos.rho_0);
+
+    printf("Material: eos: n                                    = %f\n", eos.n);
+    printf("Material: eos: shear_modulus                        = %f\n", eos.shear_modulus);
+    printf("Material: eos: young_modulus                        = %f\n", eos.young_modulus);
+
+    printf("Material: eos: polytropic_K                         = %f\n", eos.polytropic_K);
+    printf("Material: eos: polytropic_gamma                     = %f\n", eos.polytropic_gamma);
+    printf("Material: eos: bulk_modulus                         = %f\n", eos.bulk_modulus);
+    printf("Material: eos: till_A                               = %f\n", eos.till_A);
+    printf("Material: eos: till_B                               = %f\n", eos.till_B);
+    printf("Material: eos: E_0                                  = %f\n", eos.E_0);
+    printf("Material: eos: E_iv                                 = %f\n", eos.E_iv);
+    printf("Material: eos: E_cv                                 = %f\n", eos.E_cv);
+    printf("Material: eos: till_a                               = %f\n", eos.till_a);
+    printf("Material: eos: till_b                               = %f\n", eos.till_b);
+    printf("Material: eos: till_alpha                           = %f\n", eos.till_alpha);
+    printf("Material: eos: till_beta                            = %f\n", eos.till_beta);
+    printf("Material: eos: rho_limit                            = %f\n", eos.rho_limit);
+    printf("Material: eos: cs_limit                             = %f\n", eos.cs_limit);
+
     printf("-------------------------------------------------------------------------\n");
 }
 
 namespace MaterialNS {
     namespace Kernel {
+        /**
+         * @brief CUDA kernel to call Material::info() on device.
+         *
+         * @param material Pointer to the Material instance on device.
+         */
         __global__ void info(Material *material) {
             material->info();
         }
 
+        /**
+         * @brief Launches CUDA kernel to print material information.
+         *
+         * Uses custom ExecutionPolicy and cuda::launch utility.
+         *
+         * @param material Pointer to the Material instance on device.
+         */
         void Launch::info(Material *material) {
             ExecutionPolicy executionPolicy(1, 1);
             cuda::launch(false, executionPolicy, ::MaterialNS::Kernel::info, material);
         }
     }
 }
-
-
-CUDA_CALLABLE_MEMBER ArtificialViscosity::ArtificialViscosity() : alpha(0.0), beta(0.0) {
-
-}
-
-CUDA_CALLABLE_MEMBER ArtificialViscosity::ArtificialViscosity(real alpha, real beta) : alpha(alpha), beta(beta) {
-
-}
-// TODO: Add Artificial Stress? not necessary
-
-// TODO: Modify? for other EOS, not necessary
-CUDA_CALLABLE_MEMBER EqOfSt::EqOfSt() : type(0), polytropic_K(0.), polytropic_gamma(0.) {
-
-}
-// TODO: Modify? for other EOS, not necessary
-CUDA_CALLABLE_MEMBER EqOfSt::EqOfSt(int type, real polytropic_K, real polytropic_gamma) : type(type),
-                                                                                          polytropic_K(polytropic_K), polytropic_gamma(polytropic_gamma) {
-
-}
-
